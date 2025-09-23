1b2e4a277bc83398c4e3023330269909c2e6a949
Ramius-arch <colinnwambugu@gmail.com>
2025-09-23 17:10:53 +0300

    added first files of the project

---
+++ src/_init_.py
@@ -0,0 +1,9 @@
+# src/__init__.py
+
+"""
+This package provides an SDN controller designed to manage traffic dynamically in telecom networks.
+"""
+
+from .controller import SDNController
+
+__all__ = ['SDNController']
